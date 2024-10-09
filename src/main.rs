9d6b7c3349853d977832f702d94e5a7167bbef7d
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-09 16:35:17 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// Shae Sullivan
+// 20643731
+// Programming Languages - Assignment
+// 17/09/24
+
+
+fn main() { //declare main method
+    
+    let values = 1..=1000;
+    let result: Vec<_> = values.into_iter()
+        .filter(|&num| num % 2 == 0)
+        .map(|num| num * num)
+        .collect();
+
+    println!("{:?}", &result[..5]);
 }
